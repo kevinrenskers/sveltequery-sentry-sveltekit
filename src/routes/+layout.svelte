08541e3203183ebc3fad4292f8cfb9ee11e7c9e1
Kevin Renskers <kevin@loopwerk.io>
2023-08-15 14:37:01 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "../app.css";
+  import { QueryClientProvider } from "@tanstack/svelte-query";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<QueryClientProvider client={data.queryClient}>
+  <div>
+    <a href="/">Home</a>
+    <a href="/articles">Articles</a>
+  </div>
+  <main>
+    <slot />
+  </main>
+</QueryClientProvider>
